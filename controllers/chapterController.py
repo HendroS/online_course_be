4305e0479bd6e471cf5a3fa425bf5747159e3da5
HendroS <xhendrosaputrox@gmail.com>
2023-10-20 08:09:30 +0700

    add image field

--- controllers/chapterController.py
+++ controllers/chapterController.py
@@ -1,7 +1,10 @@
+import os
+from uuid import uuid4
 from flask import abort, request
 from models import Chapter,Course
 from flask_jwt_extended import current_user
-from helpers.utils import checkField
+from helpers.utils import checkField,checkValidUUID
+from werkzeug.utils import secure_filename
 
 def getAll():
     chapters=Chapter.get_all()
@@ -22,15 +25,20 @@ def softDelete(id):
     return {'msg':'soft deleted'}
 
 def create():
-    data = request.get_json()
+    # data = request.get_json()
+    data = request.form
+    image= request.files.get('image')
     required=['course_id','chapter_name','order','content']
     not_present= checkField(required,data)
     if len(not_present)>0:
         return {'msg':f'field {", ".join(not_present)} are required.'},400
     
+    if not checkValidUUID(data.get('course_id')):
+        return {'msg':'invalid course_id'},400
+
     course= Course.get_course_by_id(data.get('course_id'))
     if course == None:
-        return {'msg':'invalid course_id'},400
+        return {'msg':'course_id not found'},400
     
     chapter= Chapter(course_id=data.get('course_id'),
                      chapter_name=data.get('chapter_name'),
@@ -40,14 +48,19 @@ def create():
     if data.get('description'):
         chapter.image=data.get('description')
     
-    if data.get('image'):
-        chapter.image= data.get('image')
+    if image != None:
+        filename= secure_filename(f"{uuid4().hex}.{image.filename.split('.')[-1]}")
+        path= os.path.join('public/uploaded_img/chapter',filename)
+        image.save(path)
+        chapter.image= filename
 
     chapter.save()
     return chapter.as_dict(),201
 
 def update(id):
-    data = request.get_json()
+    # data = request.get_json()
+    data = request.form
+    image= request.files.get('image')
     required=['chapter_name','order']
     not_present= checkField(required,data)
     if len(not_present)>0:
@@ -68,9 +81,16 @@ def update(id):
     
     if data.get('description'):
         chapter.description=data.get('description')
-    
-    if data.get('image'):
-        chapter.image= data.get('image')
+
+    if image!= None:
+        old_image= chapter.image
+        filename= secure_filename(f"{uuid4().hex}.{image.filename.split('.')[-1]}")
+        path= os.path.join('public/uploaded_img/chapter',filename)
+        image.save(path)
+        chapter.image=filename
+        if old_image!=None:
+            old_path= os.path.join('public/uploaded_img/chapter',old_image)
+            os.unlink(old_path)
 
     chapter.save()
 
