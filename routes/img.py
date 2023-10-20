9f090fff4ca178c62c6dff12699b0dd77946c3ad
HendroS <xhendrosaputrox@gmail.com>
2023-10-20 08:11:17 +0700

    add route to get profile image and chapter image

--- routes/img.py
+++ routes/img.py
@@ -6,6 +6,14 @@ from flask_jwt_extended import jwt_required,get_jwt,current_user
 from flask import request, send_from_directory
 
 
-@blueprint.route('/img/<path:filename>') 
-def send_file(filename): 
-    return send_from_directory(directory='public/uploaded_img/',path=filename)
+@blueprint.route('/img/category/<path:filename>') 
+def send_category_pict(filename): 
+    return send_from_directory(directory='public/uploaded_img/category',path=filename)
+
+@blueprint.route('/img/profile/<path:filename>')
+def send_profile_pict(filename):
+    return send_from_directory(directory='public/uploaded_img/profile',path=filename)
+
+@blueprint.route('/img/chapter/<path:filename>')
+def send_chapter_image(filename):
+    return send_from_directory(directory='public/uploaded_img/chapter',path=filename)
